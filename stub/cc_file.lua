999cb3f417551595fecb8b16e0a9e9d2a0318f78
bushtail <evannosich@icloud.com>
2026-05-20 14:05:36 -0400

    Update cc_file.lua

--- stub/cc_file.lua
+++ stub/cc_file.lua
@@ -1,1 +1,21 @@
+---@class file
+local file = {}
 
+---@return string|nil
+function file.readLine() end
+
+---@return string
+function file.readAll() end
+
+---@return string|nil
+function file.read() end
+
+---@param text string
+function file.write(text) end
+
+---@param text string
+function file.writeLine(text) end
+
+function file.flush() end
+
+function file.close() end
